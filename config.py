0d5de8aebed47115f27a37822adc57bb0bddfe72
Hayko <mybot@example.com>
2026-09-15 19:02:26 +0400

    Update bot

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-sessions_folder = "/storage/emulated/0/хайдов/sessions" # Путь к папке где лежат сессии
+sessions_folder = "/storage/emulated/0/хайдов/хайдов/sessions" # Путь к папке где лежат сессии
 
-admin = [5440009063,7832222409] # Айди админов
+admin = [5905797853, 8648941987] # Айди админов
 
-api_token = "7701328204:AAG-pj8RCxmOgoYwgZh5qLS52BnrJXuykdQ" # Токен бота
+api_token = "8500453556:AAGipXXl9Z5N7T0I6nubIKPvsWotOCgOvps" # Токен бота
 
-Crypto_Pay_API_Token = "333109:AAVISyozirrFmTzgw7K5lOYRne2ijbxH0Xk" # Токен от криптобота
+Crypto_Pay_API_Token = "634746:AAo0MR5t79IMN0xsaVklaI1yxRsbgMw8krS" # Токен от криптобота
 
-log_chat_id = -1002447854876 # Айди лог чата с -100
+log_chat_id = -1004399180922 # Айди лог чата с -100
 
 count = 1 # Количество жалоб с 1 сессии, не более 20
